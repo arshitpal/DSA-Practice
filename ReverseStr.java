4805238653ebaaea49143b897c31aab4fccb8839
Arshit Pal <arshitpal408@gmail.com>
2026-09-11 13:21:16 +0530

    Add reverse string program and other DSA solutions

---
+++ ReverseStr.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class ReverseStr {
+
+    public static void main(String[] args) {
+
+        String str = "Hello, World!";
+String rev=" ";
+         for(int i=str.length()-1;i>=0;i--){
+            rev=rev+str.charAt(i);
+         }
+System.out.println(rev);
+    }
+}
---
+++ TwoSum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class TwoSum {
+
+    public static void main(String[] args) {
+
+            
+git status
+git add .
+git commit -m "Solve Two Sum"
+git push
+    }
+}
